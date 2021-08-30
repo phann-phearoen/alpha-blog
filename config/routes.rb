2156276a1c8d7b3207d7c57a60ff80c6917dbfff
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-08-30 11:15:12 +0700

    git add delete function and show-link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy] if we don't want to expose all routes
 end
