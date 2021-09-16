b4147a3fee0c83f59bc3d19df6741ae44ecccf3f
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-16 09:02:12 +0700

    add bootstrap and the welcome page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "packs/bootstrap"
+import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap";
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,2 @@
 const { environment } = require('@rails/webpacker')
-
-const webpack = require("webpack")
-
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-
-    $: 'jquery',
-
-    jQuery: 'jquery',
-
-    Popper: ['popper.js', 'default']
-
-}))
-
-export default environment
+module.exports = environment
