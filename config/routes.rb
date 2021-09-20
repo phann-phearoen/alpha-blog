2dc6cb63cde27a8914c924d1a5128c99308bb16c
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-20 08:59:25 +0700

    add user log in form and path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
+
   get 'signup', to: 'users#new'
+  
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
