24cc756a4138393a1ddd96af73eae640a914db3d
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-09-21 13:45:42 +0700

    add admin-only restriction to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -23,6 +23,14 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to category_url(Category.last)
   end
 
+  test "should not create category if not admin" do
+    assert_no_difference('Category.count') do
+      post categories_url, params: { category: { name: "Travel" } }
+    end
+
+    assert_redirected_to categories_url
+  end
+
   test "should show category" do
     get category_url(@category)
     assert_response :success
